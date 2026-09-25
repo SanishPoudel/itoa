7d09f9ee4c9a28b6d7a6c910df59940a0466f9d2
Sanish Poudel <139324676+SanishPoudel@users.noreply.github.com>
2026-09-25 20:50:31 +1000

    feat: now handles 2 digits

--- itoa.s
+++ itoa.s
@@ -1,8 +1,22 @@
 .intel_syntax noprefix
 .global itoa_digit
+.global itoa
 
 itoa_digit:
 xor rax, rax
 mov al, dil
 add al, 0x30
+ret
+
+itoa:
+xor rdx, rdx
+mov rax, rdi
+div 10
+mov rdi, rax
+call itoa_digit
+mov [rsi], al
+mov rdi, rdx
+call itoa_digit
+mov [rsi+1], al
+mov rax, 2
 ret
