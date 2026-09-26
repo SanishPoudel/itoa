0751ac3c67bd9ac4b592274637738a2a78a3a022
Sanish Poudel <139324676+SanishPoudel@users.noreply.github.com>
2026-09-26 21:19:15 +1000

    fix: now handles negatives

--- itoa.s
+++ itoa.s
@@ -8,10 +8,19 @@ mov al, dil
 add al, 0x30
 ret
 
+neg:
+mov byte ptr [rsi], 0x2d
+add rsi, 1
+neg rdi
+jmp compare
+
 itoa:
 mov r11, 0
+
+compare:
 cmp rdi, 0
 je special
+jl neg
 
 mov r8, 0
 mov r9, 0
@@ -37,7 +46,7 @@ mov r11, r8
 
 loop:
 cmp r8, 0
-je done
+je check
 pop rax
 mov [rsi+r9], al
 sub r8, 1
@@ -47,7 +56,13 @@ jmp loop
 special:
 mov byte ptr [rsi], 0x30
 mov r11, 1
-jmp done
+jmp check
+
+check:
+cmp byte ptr [rsi-1], 0x2d
+jne done
+inc r11
+sub rsi, 1
 
 done:
 mov rax, r11
