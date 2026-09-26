00dea6e596d1e6b9954d32458dceaf7106c5c20d
Sanish Poudel <139324676+SanishPoudel@users.noreply.github.com>
2026-09-26 17:08:18 +1000

    fix: resolved the leading 0 issue but needs work

--- itoa.s
+++ itoa.s
@@ -9,15 +9,33 @@ add al, 0x30
 ret
 
 itoa:
+mov r8, 0
+mov r9, 0
 xor rdx, rdx
+cmp rdi, 0
+je special
 mov rax, rdi
 mov rcx, 10
 div rcx
+cmp rax, 0
+je last
+
 mov rdi, rax
 call itoa_digit
-mov [rsi], al
+mov [rsi+r9], al
+inc r9
+inc r8
+
+last:
 mov rdi, rdx
 call itoa_digit
-mov [rsi+1], al
-mov rax, 2
+mov [rsi+r9], al
+inc r8
+mov rax, r8
+ret
+
+special:
+call itoa_digit
+mov [rsi], al
+mov rax, 1
 ret
