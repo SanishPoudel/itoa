06f19ddc4ddab41ab8e2cc48c733e73277edbe0f
Sanish Poudel <139324676+SanishPoudel@users.noreply.github.com>
2026-09-26 18:14:31 +1000

    refact: fixed everything except negatives

--- itoa.s
+++ itoa.s
@@ -9,33 +9,46 @@ add al, 0x30
 ret
 
 itoa:
-mov r8, 0
-mov r9, 0
-xor rdx, rdx
+mov r11, 0
 cmp rdi, 0
 je special
+
+mov r8, 0
+mov r9, 0
 mov rax, rdi
+
+divide:
+cmp rax, 0
+je stitch
+
+xor rdx, rdx
 mov rcx, 10
 div rcx
-cmp rax, 0
-je last
-
-mov rdi, rax
-call itoa_digit
-mov [rsi+r9], al
-inc r9
-inc r8
-
-last:
+mov r10, rax
 mov rdi, rdx
 call itoa_digit
-mov [rsi+r9], al
+push rax
+mov rax, r10
 inc r8
-mov rax, r8
-ret
+jmp divide
+
+stitch:
+mov r11, r8
+
+loop:
+cmp r8, 0
+je done
+pop rax
+mov [rsi+r9], al
+sub r8, 1
+inc r9
+jmp loop
 
 special:
-call itoa_digit
-mov [rsi], al
-mov rax, 1
+mov byte ptr [rsi], 0x30
+mov r11, 1
+jmp done
+
+done:
+mov rax, r11
 ret
